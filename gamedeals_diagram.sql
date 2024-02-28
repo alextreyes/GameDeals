50fbe2d90eb88a6ca9867dc7f90399e81ea0b19e
alex <alextreyes11@gmail.com>
2024-02-28 00:46:33 -0500

    Added everything

--- gamedeals_diagram.sql
+++ gamedeals_diagram.sql
@@ -1,62 +1,35 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/TIBpqQ
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
--- Modify this code to update the DB schema diagram.
--- To reset the sample schema, replace everything with
--- two dots ('..' - without quotes).
-
-CREATE TABLE "user" (
-    "id" int   NOT NULL,
-    "username" text  NOT NULL, 
-    "password" text   NOT NULL,
-    "profile_pic" text   NOT NULL,
-    "user_list" int   NOT NULL,
-    "liked_lists" int   NOT NULL,
-    "likes" int   NOT NULL,
-    CONSTRAINT "pk_user" PRIMARY KEY ("id")
+﻿
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    profile_pic TEXT
 );
 
-CREATE TABLE "userlist" (
-    "id" int   NOT NULL,
-    "user_id" int   NOT NULL,
-    "game_id" int   NOT NULL,
-    "likes" int   NOT NULL,
-    CONSTRAINT "pk_userlist" PRIMARY KEY ("id")
+CREATE TABLE user_lists (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    list_name TEXT NOT NULL,
+    CONSTRAINT fk_user_lists_user_id FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
-CREATE TABLE "game" (
-    "id" int   NOT NULL,
-    "name" text   NOT NULL,
-    "tags" text   NOT NULL,
-    "current_price" int   NOT NULL,
-    CONSTRAINT "pk_game" PRIMARY KEY ("id")
+CREATE TABLE user_list_games (
+    list_id INT NOT NULL,
+    game_id INT,
+    PRIMARY KEY (list_id, game_id),
+    CONSTRAINT fk_user_list_games_list_id FOREIGN KEY (list_id) REFERENCES user_lists(id) ON DELETE CASCADE
 );
 
-CREATE TABLE "likes" (
-    "id" int   NOT NULL,
-    "user_id" int   NOT NULL,
-    "list_id" int   NOT NULL,
-    CONSTRAINT "pk_likes" PRIMARY KEY ("id")
+CREATE TABLE likes (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    list_id INT NOT NULL,
+    CONSTRAINT fk_likes_user_id FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    CONSTRAINT fk_likes_list_id FOREIGN KEY (list_id) REFERENCES user_lists(id) ON DELETE CASCADE
 );
 
-ALTER TABLE "user" ADD CONSTRAINT "fk_user_userlist" FOREIGN KEY ("user_list")
-REFERENCES "userlist" ("id");
-
-ALTER TABLE "userlist" ADD CONSTRAINT "fk_userlist_user_id" FOREIGN KEY ("user_id")
-REFERENCES "user" ("id");
-
-ALTER TABLE "userlist" ADD CONSTRAINT "fk_userlist_game_id" FOREIGN KEY ("game_id")
-REFERENCES "game" ("id");
-
-ALTER TABLE "likes" ADD CONSTRAINT "fk_likes_user_id" FOREIGN KEY ("user_id")
-REFERENCES "user" ("id");
-
-ALTER TABLE "likes" ADD CONSTRAINT "fk_likes_list_id" FOREIGN KEY ("list_id")
-REFERENCES "userlist" ("id");
-
-CREATE INDEX "idx_user_username"
-ON "user" ("username");
+CREATE INDEX idx_users_username ON users (username);
+CREATE INDEX idx_user_lists_user_id ON user_lists (user_id);
 
 
 
